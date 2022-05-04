77b13188e610d0d1741c9c10b9de9583b2aa7aaa
HibaMallak <hiba.mallak@technion.ac.il>
2022-05-04 20:07:38 +0300

    player

---
+++ Player.h
@@ -0,0 +1,20 @@
+struct Player{
+    char* name;
+    int level;
+    int force;
+    int maxHP;
+    int HP;
+    int coins;
+    public:
+    Player( char* name,int maxHP, int force);
+}
+
+Player( char* name,int maxHP=100, int force=5)
+{
+    this.name= name;
+    this.maxHP= maxHP;
+    this.force= force;
+    this.coins=0;
+    this.HP= maxHP;
+    this.level=0;
+}

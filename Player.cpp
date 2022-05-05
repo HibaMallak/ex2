1034e489b9c51911d92a3abad1bb02e2867cb429
HibaMallak <hiba.mallak@technion.ac.il>
2022-05-06 00:36:33 +0300

    LastUpdate

--- Player.cpp
+++ Player.cpp
@@ -1,28 +1,156 @@
+
 #include "Player.h"
+#define DEFAULT_FORCE 5
+#define DEFAULT_MAXHP 100
+#define FIRST 1
+#define MAX_LEVEL 10
 
 
-
-Player::Player( char* name,int maxHP=100, int force=5)
+Player::Player( char* name,int maxHP=DEFAULT_MAXHP, int force=DEFAULT_FORCE)
 {
-    this->name= name;
-    this->maxHP= maxHP;
-    this->force= force;
-    this->coins=0;
-    this->HP= maxHP;
-    this->level=0;
+    this->m_name= name;
+    if(maxHP>=0)
+    {
+        this->m_maxHP= maxHP;
+    }
+    else
+    {
+        this->m_maxHP= DEFAULT_MAXHP;
+    }
+    if(force>=0)
+    {
+        this->m_force= force;
+    }
+    else
+    {
+        this->m_force= DEFAULT_FORCE;
+    }
+    this->m_HP= maxHP;
+    this->m_level= FIRST;
 }
 
 Player::Player(const Player& player)
 {
-    this->name= player.name;
-    this->maxHP= player.maxHP;
-    this->force= player.force;
-    this->coins= player.coins;
-    this->HP= player.maxHP;
-    this->level= player.level;
+    this->m_name= player.m_name;
+    this->m_maxHP= player.m_maxHP;
+    this->m_force= player.m_force;
+    this->m_coins= player.m_coins;
+    this->m_HP= player.m_maxHP;
+    this->m_level= player.m_level;
 }
 
 Player::~Player()
 {
 
 }
+
+Player& Player::operator=(const Player p)
+{
+    this->m_name= p.m_name;
+    this->m_maxHP= p.m_maxHP;
+    this->m_force= p.m_force;
+    this->m_coins= p.m_coins;
+    this->m_HP= p.m_maxHP;
+    this->m_level= p.m_level;
+    return *this;
+}
+void Player::printInfo()
+{
+    printPlayerInfo(this->m_name, this->m_level, this->m_force, this->m_HP, this->m_coins);
+}
+
+void Player::levelUp()
+{
+    if(this->m_level!= MAX_LEVEL)
+    {
+        this->m_level++;
+    }
+}
+
+int Player::getLevel()
+{
+    return this->m_level;
+}
+
+void Player::buff(int forceToAdd)
+{
+    if(forceToAdd>0)
+    {
+        this->m_force += forceToAdd;
+    } 
+}
+
+void Player::heal(int pointsToHeal)
+{
+    if(pointsToHeal>0)
+    {
+        if(this->m_HP+ pointsToHeal > this->m_maxHP)
+        {
+            this->m_HP= this->m_maxHP;
+        }
+        else
+        {
+            this->m_HP += pointsToHeal;
+        }
+    }
+}
+
+void Player::damage(int damageToMake)
+{
+    if(damageToMake>0)
+    {
+        if(this->m_HP - damageToMake <0)
+        {
+            this->m_HP =0;
+        }
+        else
+        {
+            this->m_HP -= damageToMake;
+        }
+    }
+}
+
+bool Player::isKnockedOut()
+{
+    if(!(this->m_HP))
+    {
+        return true;
+    }
+    return false;
+}
+
+void Player::addCoins(int coinsToAdd)
+{
+    if(coinsToAdd>0)
+    {
+        this->m_coins += coinsToAdd;
+    }
+}
+
+bool Player::pay(int coinsToPay)
+{
+    if(coinsToPay>0)
+    {
+        if(this->m_coins - coinsToPay <0)
+        {
+            return false;
+        }
+        else
+        {
+            this->m_coins -= coinsToPay;
+            return true;
+        }
+    }
+    return true;
+}
+
+int Player::getAttackStrength()
+{
+    return this->m_force + this->m_level;
+}
+
+
+
+
+
+
--- Player.h
+++ Player.h
@@ -1,29 +1,154 @@
-class Player
-{
-private:
-    char* name;
-    int level;
-    int force;
-    int maxHP;
-    int HP;
-    int coins;
+#ifndef EX2_Player_H
+#define EX2_Player_H
+#include "utilities.h"
+
+class Player{
 public:
+    /*
+     * C'tor of Player class
+     *
+     * @param name - The name of the player.
+     * @param maxHP - The maxHP of the player.
+     * @return
+     *      A new instance of Player.
+    */
     Player(char* name,int maxHP, int force);
+
+
+    /*
+     * Copy c'tor of Player class
+     *
+     * @param p - The player to copy construct from.
+     * @return
+     *      A copied instance of p.
+    */
+    Player(const Player& p);
+
+
+    /*
+     * D'tor of Player class
+     *
+     * @return
+     *      Deletes this instance of Player.
+    */
     ~Player();
+
+
+    /*
+     * Assignment operator
+     *
+     * @param p - The player to assign from.
+     * @return  
+     *      ///////
+    */
+    Player& operator=(const Player p);
+
+
+    /*
+     * Prints the player info:
+     *
+     * @return
+     *      void
+    */
+    void printInfo();
+
+
+    /*
+     * Increases the level of the player by one.
+     *
+     * @return
+     *      void
+    */
+    void levelUp();
+
+
+    /*
+     * Returns the level of the player.
+     *
+     * @return
+     *      The level of the player.
+    */
+    int getLevel();
+
+
+    /*
+     * Increases the force of the player by given amount.
+     *
+     * @param forceToAdd - The given amount to add to the force of the player.
+     * @return
+     *      void
+    */
+    void buff(int forceToAdd);
+
+
+    /*
+     * Increases the HP of the player by given amount of points.
+     *
+     * @param printsToHeal - the amount of points to add to the HP of the player.
+     * @return
+     *      void
+    */
+    void heal(int pointsToHeal);
+
+
+    /*
+     * Decreases the HP of the player by given amount of points.
+     *
+     * @param damageToDeal - the amount of points to reduce from the HP of the player.
+     * @return
+     *      void
+    */
+    void damage(int damageToDeal);
+
+
+    /*
+     * Check if the player is knocked out:
+     *
+     * @return
+     *      True if player is knocked out
+     *      False otherwise
+    */
+    bool isKnockedOut();
+
+
+    /*
+     * Increases the coins of the player by given amount of coins.
+     *
+     * @param coinsToAdd - the amount of coins to add to the coins of the player.
+     * @return
+     *      void
+    */
+    void addCoins(int coinsToAdd);
+
+
+    /*
+     * Check if player can pay the given amount of coins, and pays if possible.
+     *
+     * @param coinsToPay - the amount of coins to pay.
+     * @return
+     *      True if player can pay
+     *      False otherwise
+    */
+    bool pay(int coinsToPay);
+
+
+    /*
+     * Returns the attack strenth, which is defined as force + level of the player.
+     *
+     * @return
+     *      The attack strenth of the player.
+    */
+    int getAttackStrength();
+
+private:
+    char* m_name;
+    int m_level;
+    int m_force;
+    int m_maxHP;
+    int m_HP;
+    int m_coins;
+
 };
 
-Player::~Player()
-{
-}
-    
-}
 
-Player::Player( char* name,int maxHP=100, int force=5)
-{
-    this.name= name;
-    this.maxHP= maxHP;
-    this.force= force;
-    this.coins=0;
-    this.HP= maxHP;
-    this.level=0;
-}
+#endif

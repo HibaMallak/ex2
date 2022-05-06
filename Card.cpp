adf4a1430ee8e41b45e256d0d0fb17cd00739479
shaniamsalem <102696344+shaniamsalem@users.noreply.github.com>
2022-05-06 17:03:09 +0300

    adding card.cpp

---
+++ Card.cpp
@@ -0,0 +1,92 @@
+#include "Card.h"
+#define WIN true
+#define LOSS false
+
+Card::Card(CardType type, const CardStats &stats)
+{
+    this->m_effect=type;
+
+    if (type == CardType::Battle)
+    {
+        this->m_stats.loot=stats.loot;
+        this->m_stats.force=stats.force;
+        this->m_stats.hpLossOnDefeat=stats.hpLossOnDefeat;
+    }
+
+    if (type == CardType::Buff)
+    {
+        this->m_stats.cost=stats.cost;
+        this->m_stats.buff=stats.buff;
+    }
+
+
+    if (type == CardType::Treasure)
+    {
+        this->m_stats.loot=stats.loot;
+    }
+
+    if (type == CardType::Heal)
+    {
+        this->m_stats.cost=stats.cost;
+        this->m_stats.heal=stats.heal;
+    }
+}
+
+void Card::applyEncounter(Player& player) const
+{
+
+    if (this->m_effect== CardType::Battle)
+    {
+        if (this->m_stats.force <= player.getAttackStrength())
+        {
+            player.levelUp();
+            player.addCoins(this->m_stats.loot) ;
+            printBattleResult(WIN);
+        }
+        else
+        {
+            player.damage(this->m_stats.hpLossOnDefeat);
+            printBattleResult(LOSS);
+        }
+    }
+
+    if (this->m_effect== CardType::Heal && player.pay(this->m_stats.cost))
+    {
+            player.heal(this->m_stats.heal);
+    }
+
+    if (this->m_effect== CardType::Buff && player.pay(this->m_stats.cost))
+    {
+        player.buff(this->m_stats.force);
+    }
+
+    if (this->m_effect== CardType::Treasure)
+    {
+        player.addCoins(this->m_stats.loot);
+    }
+}
+
+void Card:: printInfo() const
+{
+
+    if (this->m_effect== CardType::Treasure)
+    {
+        printTreasureCardInfo(this->m_stats);
+    }
+
+    if (this->m_effect== CardType::Battle)
+    {
+        printBattleCardInfo(this->m_stats);
+    }
+
+    if (this->m_effect== CardType::Buff)
+    {
+        printBuffCardInfo(this->m_stats);
+    }
+
+    if (this->m_effect== CardType::Heal)
+    {
+        printHealCardInfo(this->m_stats);
+    }
+
+}

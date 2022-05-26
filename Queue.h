0e216f43227f7ee20380e8bbe1780bea1e695765
shaniamsalem <102696344+shaniamsalem@users.noreply.github.com>
2022-05-26 14:27:26 +0300

    Add files via upload

--- Queue.h
+++ Queue.h
@@ -1,106 +1,117 @@
 #ifndef EX3_QUEUE_H
 #define EX3_QUEUE_H
 
-static const int INITIAL_SIZE = 0;
 template <typename T>
 class Queue
 {
-    private:
-        struct Node
-        {
-            T m_data;
-            Node *m_nextNode;
-            Node::Node(T data)
-            {
-                this->m_data= data;
-                this->m_nextNode= nullptr;
-            }
-            Node::Node(const Node& node)
-            {
-                Node newNode= new Node(node->m_data);
-            }
-            Node& Node::operator=(const Node& node)
-            {
-                this->m_data= node->m_data;
-                this->m_nextNode= node->m_nextNode;
-            }
-            Node::Node~Node()
-            {
-                delete *this;
-            }
-        };
-        Node *m_head;
-        Node *m_tail;
-        int m_size;
+private:
+    static const int INITIAL_SIZE = 0;
+    struct Node
+    {
+        T m_data;
+        Node *m_nextNode= nullptr;
+    };
 
-    public:
-        Queue::Queue<T>(): this->m_size= INITIAL_SIZE,
-                           this->m_head= new Node(nullptr), 
-                           this->m_tail= this->m_head 
-        {
+    Node *m_head;
+    Node *m_tail;
+    int m_size;
 
-        }
+public:
 
-        Queue::Queue<T>(const Queue<T> &q)
-        {
-            Queue<T> newQueue= new Queue<T>();
-
-        }
-
-        Queue& Queue::operator=(Queue& q)
-        {
-            if(*this== q)
-            {
-                return *this;
-            }
-            int* temp =Queue();
-            //unfinished
-        }
-
-        ~Queue()
-        {
-            if(this->m_size < INITIAL_SIZE)
-            {
-                return;
-            }
-            this->m_head.~Queue();
-            this->m_head.~Node();
-
-        }
-
-        int Queue::pushBack(T& data)
-        {
-            this->m_tail->m_nextNode= new Node(data);
-            this->m_tail= this->m_tail->m_nextNode;
-            ++this->m_size;
-        }
-
-        T& Queue::front()
-        {
-            return (this->m_head->m_nextNode)->m_data;
-        }
-
-        void Queue::popFront()
-        {
-            //שגיאה=חריגה
-            /*if(this->m_size == INITIAL_SIZE)
-            {
-                Queue temp= this->m_tail;
-                this->m_tail= this->m_head;
-            }*/
-            Queue temp= new Queue<T>(); 
-            temp->m_tail= this->m_head->m_nextNode();
-            this->m_head->m_nextNode= (this->m_head->m_nextNode)->m_nextNode;
-            --this->m_size;
-            temp.~Queue();
-        }
-
-        int& Queue::size()
-        {
-            return this->m_size;
-        }
+    Queue();
+    Queue(const Queue<T> &q);
+    ~Queue();
+    Queue& operator=(const Queue& q);
+    void pushBack(const T& data);
+    T& front();
+    void popFront();
+    int size() const;
 
 };
+
+
+template<typename T>
+Queue<T>::Queue() : m_size(INITIAL_SIZE), m_head(new Node<T>()), m_tail(this->m_head)
+{
+
+}
+
+
+template <typename T>
+Queue<T>::Queue(const Queue<T>& q)
+{
+    // do with iterator & pushback
+    // build a new one
+}
+
+template <typename T>
+Queue<T>& Queue<T>::operator=(const Queue<T>& q)
+{
+    if(*this == &q)
+    {
+        return *this;
+    }
+
+    //do pop until rek - push - iterator (loop)
+}
+
+template <typename T>
+Queue<T>::~Queue()
+{
+    while (this->m_head)
+    {
+        Node<T>* toDelete = this-> m_head;
+        m_head=m_head->m_nextNode;
+        delete toDelete;
+    }
+
+    //  delete m_head; //??
+    // delete m_tail; //??
+}
+template <typename T>
+void Queue<T>:: pushBack(const T& data)
+{
+    this->m_tail->m_nextNode= new Node<T>();
+    this->m_tail= this->m_tail->m_nextNode;
+    this->m_tail->m_data=data;
+    ++this->m_size;
+}
+
+template <typename T>
+T& Queue<T>:: front()
+{
+    if (m_size == 0)
+        // throw
+    return this->m_head->m_nextNode->m_data;
+}
+
+template <typename T>
+void Queue<T>:: popFront()
+{
+    if(this->m_size == INITIAL_SIZE)
+    {
+        // throw
+    }
+
+    Node<T>* toDelete = this-> m_head.m_nextNode;
+
+    if (toDelete == this->m_tail)
+    {
+        this->m_tail= this->m_head;
+    }
+
+    m_head->m_nextNode=m_head->m_nextNode->m_nextNode;
+     delete toDelete;
+    --this->m_size;
+
+}
+
+template <typename T>
+int Queue<T>:: size() const
+{
+    return this->m_size;
+}
+
 
 template <typename T, typename S>
 Queue<T> filter(Queue<T>& q, S func)
